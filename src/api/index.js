d60a8ce70c307d0cf74bd4839592fdaba8e50be4
prathamesh22davande <prathamesh22davande@gmail.com>
2021-01-10 17:20:44 +0530

    Completed line chart for overall cases and deaths

--- src/api/index.js
+++ src/api/index.js
@@ -20,3 +20,18 @@ export const fetchData = async () => {
     console.log(error);
   }
 };
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    //console.log(data);
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+    return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
